098646e66bc2d64838831484994f6ce6865c42fc
zawminmyat <zawminmyat@localhost>
2024-01-23 15:46:49 +0630

    first add

---
+++ time-off-management/src/main/java/yu/cs/spring/TimeOffManagementApplication.java
@@ -0,0 +1,13 @@
+package yu.cs.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOffManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeOffManagementApplication.class, args);
+	}
+
+}
---
+++ time-off-management/src/test/java/yu/cs/spring/TimeOffManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.cs.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeOffManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
