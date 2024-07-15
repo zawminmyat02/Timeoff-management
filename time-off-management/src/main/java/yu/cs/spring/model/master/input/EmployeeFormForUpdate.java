92659e56b04d77cb51b3ea2cf7a25dc8f7bd6506
Wai Yan Lin <waiyanlin2015015@gmail.com>
2024-07-16 01:27:54 +0630

    -update employee

--- time-off-management/src/main/java/yu/cs/spring/model/master/input/EmployeeFormForUpdate.java
+++ time-off-management/src/main/java/yu/cs/spring/model/master/input/EmployeeFormForUpdate.java
@@ -1,35 +1,28 @@
 package yu.cs.spring.model.master.input;
 
-import java.time.LocalDate;
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import yu.cs.spring.model.master.entity.Employee;
-import yu.cs.spring.model.master.entity.Employee.Gender;
+import yu.cs.spring.model.master.entity.Employee.Status;
 
 public record EmployeeFormForUpdate(
+		String code,
 		@NotBlank(message = "Please enter employee name.")
 		String name,
 		@NotBlank(message = "Please enter phone.")
 		String phone,
 		@NotBlank(message = "Please enter email.")
-		String email,
-		@NotNull(message = "Please select gender.")
-		Gender gender,
-		@NotNull(message = "Please enter date of birth.")
-		LocalDate dob,
-		@NotNull(message = "Please enter assign date.")
-		LocalDate assignDate,
+		String email,		
+		Status status,
 		String remark) {
 
 	public static EmployeeFormForUpdate from(Employee entity) {
 		return new EmployeeFormForUpdate(
+				entity.getCode(),
 				entity.getAccount().getName(), 
 				entity.getPhone(), 
 				entity.getEmail(), 
-				entity.getGender(), 
-				entity.getDateOfBirth(), 
-				entity.getAssignDate(), 
+				entity.getStatus(),
 				entity.getRemark());
 	}
 }
+
